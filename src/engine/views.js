3f2f5dd615cb5c631e7421455589eb3a1f67598e
Alberto <albertoalbertomax@hotmail.com>
2020-02-09 16:23:07 +0100

    app alias

--- src/engine/views.js
+++ src/engine/views.js
@@ -1,9 +1,9 @@
 require('module-alias/register')
 
 //Here defines components
-const  {AppComponent} = require('../app/app-component.js');
-const  {MenuComponent} = require('../app/menu/menu-component.js');
-const  {ViewComponent} = require('../app/view/view-component.js');
+const  {AppComponent} = require('@app/app-component.js');
+const  {MenuComponent} = require('@app/menu/menu-component.js');
+const  {ViewComponent} = require('@app/view/view-component.js');
 customElements.define ('app-root', AppComponent);
 customElements.define ('menu-component', MenuComponent);
 customElements.define ('view-component', ViewComponent);
